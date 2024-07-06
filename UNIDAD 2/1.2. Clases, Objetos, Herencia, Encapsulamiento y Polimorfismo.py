93284ce5e18751d27651a4cb7cb8b351c2a093b8
wrnunezz <wr.nunezz@uea.edu.ec>
2024-07-06 11:12:11 -0500

    Clase Encuentro del 06_07_2024

--- UNIDAD 2/1.2. Clases, Objetos, Herencia, Encapsulamiento y Polimorfismo.py
+++ UNIDAD 2/1.2. Clases, Objetos, Herencia, Encapsulamiento y Polimorfismo.py
@@ -1,37 +1,44 @@
-#class Vehiculo
 class Vehiculo:
     def __init__(self, marca, modelo):
         self.__marca = marca  # privada
-        self.__modelo = modelo # privado
+        self.__modelo = modelo  # privado
+
     def descripcion(self):
-        return f'{self.__marca} {self.__modelo} '
+        return f'{self.__marca} {self.__modelo}'
+
 
 class Auto(Vehiculo):
     def __init__(self, marca, modelo, cantidad_llantas):
-        super().__init__(marca,modelo)
+        super().__init__(marca, modelo)
         self.cantidad_llantas = cantidad_llantas
 
     def descripcion(self):
-        return f'{self.marca} {self.modelo} '
+        return f'{self._Vehiculo__marca} {self._Vehiculo__modelo}'
+
 
 class Camion(Vehiculo):
     def __init__(self, marca, modelo, capacidad_carga):
-        super().__init__(marca,modelo)
+        super().__init__(marca, modelo)
         self.capacidad_carga = capacidad_carga
+
     def descripcion(self):
-        return f'{self.marca} {self.modelo} '
-#ejemplo del polimorfismo
+        return f'{self._Vehiculo__marca} {self._Vehiculo__modelo}'
+
+
+# Ejemplo del polimorfismo
 def imprimir_descripcion(vehiculo):
     print(vehiculo.descripcion())
 
-#INTANCIAR OBJETOS
-auto1 = Auto("Toyota","Hilux",2)
+
+# Instanciar objetos
+auto1 = Auto("Toyota", "Hilux", 2)
 print(auto1.descripcion())
-auto2 = Auto("Mazda","CX3",5)
+auto2 = Auto("Mazda", "CX3", 5)
 print(auto2.descripcion())
 
-## llamada al polimorfismo
+# Llamada al polimorfismo
 imprimir_descripcion(auto1)
+imprimir_descripcion(auto2)
 
 
 
